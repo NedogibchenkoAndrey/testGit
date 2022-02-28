5ee02194e72f6f6d5f29c4505c28881ecf860740
Недогибченко Андрей Сергеевич <ASNedogibchenko@sberbank.ru>
2022-02-28 10:09:45 +0300

    branch branch1, commit 3, change 3

--- src/com/Run3.java
+++ src/com/Run3.java
@@ -2,6 +2,6 @@ package com;
 
 public class Run3 {
     public static void main(String[] args) {
-        System.out.println("branch branch1, commit 3, change 2");
+        System.out.println("branch branch1, commit 3, change 3");
     }
 }
